ee1b9d494eace0e7c482a99c110a36d006cdfcb1
olivier-tan-ics4u1-02-25 <olivier.tan@student.tdsb.on.ca>
2025-03-30 22:05:28 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,24 +4,17 @@ public class PracticeProblem {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+	// 1. Create a method called bubbleSortString(String[] strings) which takes in a string array and sorts it in alphabetical order.  Your bubble sort should ignore casing.  return nothing.
 
-	public static void q2() {
-		//Write question 2 code here
+	public static void bubbleSortString(String [] strings){
+		for (int i = 0; i <strings.length; i++){
+			for (int j = 0; j < strings.length-1; j++){
+				if (strings[j].compareToIgnoreCase(strings[j+1])>0){
+					String temp = strings [j];
+					strings [j] = strings [j+1];
+					strings [j+1] = temp;
+				}
+			}
+		}
 	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
